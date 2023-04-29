1376d677d7883baa7f48849d939a671254a4209b
Peter Durham <peterdurham@protonmail.com>
2023-04-28 23:24:17 -0700

    finished up about series 1 page

--- pages/series-1.js
+++ pages/series-1.js
@@ -2,13 +2,41 @@ import Layout from "@/components/layout";
 import styled from "styled-components";
 
 const ContainerWrapper = styled.div`
-  width: 600px;
+  width: 480px;
   margin: 60px auto;
 
   h1 {
     margin-bottom: 10px;
   }
-  .italic
+
+  .pack-info {
+    display: flex;
+    margin-bottom: 40px;
+  }
+  .pack-text {
+    width: 60%;
+  }
+  .pack-image {
+    width: 40%;
+    padding-top: 10px;
+  }
+  @media (max-width: 720px) {
+    & {
+      width: calc(100% - 80px);
+      margin-left: 40px;
+      margin-right: 40px;
+    }
+    .pack-text {
+      width: 100%;
+      margin-bottom: 40px;
+    }
+    .pack-image {
+      width: 100%;
+    }
+    .pack-info {
+      flex-direction: column;
+    }
+  }
 `;
 
 const SeriesOne = () => {
@@ -20,38 +48,62 @@ const SeriesOne = () => {
           Released November 10, 2022
         </p>
 
-        <h2 style={{ marginBottom: "10px" }}>Orange Pill in a pack</h2>
+        <div className="pack-info">
+          <div className="pack-text">
+            <h2 style={{ marginBottom: "10px" }}>Orange Pill in a pack</h2>
 
-        <p style={{ fontWeight: "700" }}>Packs include:</p>
-        <p>1x Rare, Ultra-rare, Legendary, or 1/1</p>
-        <p>9x Commons</p>
+            <p>
+              328 booster boxes - <span className="italic">24 packs each</span>
+            </p>
+            <p style={{ marginBottom: "10px" }}>7,894 packs</p>
+            <p style={{ fontWeight: "700" }}>Packs include:</p>
+            <p>1x Rare, Ultra-rare, Legendary, or 1/1</p>
+            <p>9x Series 1 Commons</p>
+          </div>
+          <img
+            className="pack-image"
+            src="/images/packs/unopened_s1_pack.jpg"
+            style={{
+              width: "100px",
+              height: "142.5px",
+              cursor: "pointer",
+              margin: "0 auto",
+            }}
+          />
+        </div>
 
-        <p style={{ fontWeight: "700", marginTop: "20px" }}>Print run:</p>
-        <p>
-          328 boxes - <span className="italic">24 packs each</span>
-        </p>
-
-        <p style={{ marginBottom: "10px" }}>7,894 packs</p>
-        <h2 style={{ marginTop: "40px", marginBottom: "20px" }}>
-          Pacific Bitcoin pack
-        </h2>
-        <p style={{ fontWeight: "700", marginTop: "20px" }}>Packs include:</p>
-        <p>1x Pacific Bitcoin Rare</p>
-        <p>9x Series 1 commons</p>
-
-        <p style={{ fontWeight: "700", marginTop: "20px" }}>Print run:</p>
-        <p>1,998 Pacific Bitcoin packs</p>
+        <div className="pack-info">
+          <div className="pack-text">
+            <h2 style={{ marginBottom: "10px" }}>Pacific Bitcoin pack</h2>
+            <p style={{ marginBottom: "10px" }}>1,998 packs</p>
+            <p style={{ fontWeight: "700", marginTop: "10px" }}>
+              Packs include:
+            </p>
+            <p>1x Pacific Bitcoin Rare</p>
+            <p>9x Series 1 Commons</p>
+          </div>
+          <img
+            className="pack-image"
+            src="/images/packs/unopened_pb2022_pack.jpg"
+            style={{
+              width: "100px",
+              height: "142.5px",
+              cursor: "pointer",
+              margin: "0 auto",
+            }}
+          />
+        </div>
 
         <h2 style={{ marginTop: "40px", marginBottom: "10px" }}>
           Total Printed
         </h2>
-        <p>98,920 - total cards</p>
-        <p>88,913 - commons</p>
-        <p>5,994 - rares</p>
-        <p>1,500 - ultra-rares</p>
-        <p>500 - legendaries</p>
-        <p>42 - lost cards</p>
-        <p>15 - unique 1/1 cards</p>
+        <p>98,920 - Total cards</p>
+        <p>88,913 - Commons</p>
+        <p>5,994 - Rares</p>
+        <p>1,500 - Ultra-rares</p>
+        <p>500 - Legendaries</p>
+        <p>42 - Lost cards</p>
+        <p>15 - Unique 1/1 cards</p>
       </ContainerWrapper>
     </Layout>
   );
